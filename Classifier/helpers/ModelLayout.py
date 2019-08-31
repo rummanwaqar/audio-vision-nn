85ed953cb511fff23a9d5ce8c1192c7820420d0e
Megnath <megnath@ualberta.ca>
2019-08-31 15:13:56 -0600

    Completed model code

--- Classifier/helpers/ModelLayout.py
+++ Classifier/helpers/ModelLayout.py
@@ -12,45 +12,34 @@ import torch.nn.functional as F
 from sklearn import metrics 
 
 class AudioVisionModel(nn.Module):
+    num_rows = 14
+    num_columns = 174
+    num_channels = 1
+
      def __init__(self):
         super(Model, self).__init__()
         self.seq1 = nn.Sequential()
-        self.conv1 = nn.Conv2d(1, 20, 5)
-        self.conv2 = nn.Conv2d(20, 20, 5)
+        self.conv1 = nn.Conv2d(1, 16, 2)
+        self.conv2 = nn.Conv2d(16, 32, 2)
+        self.conv3 = nn.Conv2d(32, 64, 2)
+        self.conv4 = nn.Conv2d(64, 128, 2)
 
-    def forward(self, x):
-        x = F.relu(self.conv1(x))
-        return F.relu(self.conv2(x))
+        self.drop1 = nn.Dropout(p=0.2)
+        self.drop2 = nn.Dropout(p=0.2)
+        self.drop3 = nn.Dropout(p=0.2)
+        self.drop4 = nn.Dropout(p=0.2)
 
+        self.maxpool1 = nn.MaxPool2d(2)
+        self.maxpool2 = nn.MaxPool2d(2)
+        self.maxpool3 = nn.MaxPool2d(2)
+        self.maxpool4 = nn.MaxPool2d(2)
 
+        self.avgPool = nn.AvgPool2d(2)
+        self.dense = nn.Softmax(2)
 
-num_rows = 14
-num_columns = 174
-num_channels = 1
-
-x_train = x_train.reshape(x_train.shape[0], num_rows, num_columns, num_channels)
-x_test = x_test.reshape(x_test.shape[0], num_rows, num_columns, num_channels)
-
-num_labels = yy.shape[1]
-filter_size = 2
-
-# Construct model 
-model = Sequential()
-model.add(Conv2D(filters=16, kernel_size=2, input_shape=(num_rows, num_columns, num_channels), activation='relu'))
-model.add(MaxPooling2D(pool_size=2))
-model.add(Dropout(0.2))
-
-model.add(Conv2D(filters=32, kernel_size=2, activation='relu'))
-model.add(MaxPooling2D(pool_size=2))
-model.add(Dropout(0.2))
-
-model.add(Conv2D(filters=64, kernel_size=2, activation='relu'))
-model.add(MaxPooling2D(pool_size=2))
-model.add(Dropout(0.2))
-
-model.add(Conv2D(filters=128, kernel_size=2, activation='relu'))
-model.add(MaxPooling2D(pool_size=2))
-model.add(Dropout(0.2))
-model.add(GlobalAveragePooling2D())
-
-model.add(Dense(num_labels, activation='softmax'))
+    def forward(self, input):
+        x = self.drop1(self.maxpool1(F.relu(self.conv1(input)))
+        x = self.drop2(self.maxpool2(F.relu(self.conv2(input)))
+        x = self.drop3(self.maxpool3(F.relu(self.conv3(input)))
+        x = self.drop4(self.maxpool4(F.relu(self.conv4(input)))
+        return (self.dense(self.avgPool(x)))
