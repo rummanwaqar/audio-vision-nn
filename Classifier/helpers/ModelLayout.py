f9b2ce53033838cd56c7f58fcfccc0aedd8f9a46
Megnath <megnath@ualberta.ca>
2019-09-02 12:40:21 -0600

    Model compiles with 2 layers

--- Classifier/helpers/ModelLayout.py
+++ Classifier/helpers/ModelLayout.py
@@ -2,27 +2,18 @@ import numpy as np
 import torch.nn as nn
 import torch.nn.functional as F
 
-
-
-# from keras.models import Sequential
-# from keras.layers import Dense, Dropout, Activation, Flatten
-# from keras.layers import Convolution2D, Conv2D, MaxPooling2D, GlobalAveragePooling2D
-# from keras.optimizers import Adam
-# from keras.utils import np_utils
-from sklearn import metrics 
-
 class AudioVisionModel(nn.Module):
     num_rows = 14
     num_columns = 174
     num_channels = 1
 
-     def __init__(self):
-        super(Model, self).__init__()
+    def __init__(self):
+        super(AudioVisionModel, self).__init__()
         self.seq1 = nn.Sequential()
-        self.conv1 = nn.Conv2d(1, 16, 2)
-        self.conv2 = nn.Conv2d(16, 32, 2)
-        self.conv3 = nn.Conv2d(32, 64, 2)
-        self.conv4 = nn.Conv2d(64, 128, 2)
+        self.conv1 = nn.Conv2d(1, 14, 2)
+        self.conv2 = nn.Conv2d(14, 28, 2)
+        self.conv3 = nn.Conv2d(28, 56, 2)
+        self.conv4 = nn.Conv2d(56, 112, 2)
 
         self.drop1 = nn.Dropout(p=0.2)
         self.drop2 = nn.Dropout(p=0.2)
@@ -37,9 +28,10 @@ class AudioVisionModel(nn.Module):
         self.avgPool = nn.AvgPool2d(2)
         self.dense = nn.Softmax(2)
 
-    def forward(self, input):
-        x = self.drop1(self.maxpool1(F.relu(self.conv1(input)))
-        x = self.drop2(self.maxpool2(F.relu(self.conv2(input)))
-        x = self.drop3(self.maxpool3(F.relu(self.conv3(input)))
-        x = self.drop4(self.maxpool4(F.relu(self.conv4(input)))
-        return (self.dense(self.avgPool(x)))
+    def forward(self, x):
+
+        out1 = self.drop1(self.maxpool1(F.relu(self.conv1(x))))
+        out2 = self.drop2(self.maxpool2(F.relu(self.conv2(out1))))
+        # out3 = self.drop3(self.maxpool3(F.relu(self.conv3(out2))))
+        # out4 = self.drop4(self.maxpool4(F.relu(self.conv4(out3))))
+        return (self.dense(self.avgPool(out2)))
