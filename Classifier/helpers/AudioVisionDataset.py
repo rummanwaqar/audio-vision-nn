671dc1138c2f21c1eaed648b01440d1a1192d125
Megnath <megnath@ualberta.ca>
2019-09-04 23:43:33 -0600

    Switched to conv1d to account for the data format

--- Classifier/helpers/AudioVisionDataset.py
+++ Classifier/helpers/AudioVisionDataset.py
@@ -10,10 +10,9 @@ class AudioVisionDataset(data.Dataset):
         self.data = featuresdf['feature'].apply(literal_eval)
         self.target = featuresdf['siren_present']
         self.n_samples = self.data.shape[0]
-        self.classes = {'false': 0, 'true': 1 }
-    
+        
     def __len__(self):   # Length of the dataset.
         return self.n_samples
     
     def __getitem__(self, index):   # Function that returns one point and one label.
-        return torch.Tensor(self.data[index]), torch.Tensor(self.target[index])
+        return torch.Tensor(self.data[index]), torch.Tensor(self.target[index], dtype=torch.long)
--- Classifier/helpers/ModelLayout.py
+++ Classifier/helpers/ModelLayout.py
@@ -9,21 +9,27 @@ class AudioVisionModel(nn.Module):
 
     def __init__(self):
         super(AudioVisionModel, self).__init__()
-        self.seq1 = nn.Sequential()
-        self.conv1 = nn.Conv2d(1, 14, 2)
-        self.conv2 = nn.Conv2d(14, 28, 2)
+
+        self.conv1 = nn.Conv1d(1, 14, 1)
+        self.conv2 = nn.Conv1d(14, 28, 1)
 
         self.drop1 = nn.Dropout(p=0.2)
-        self.drop2 = nn.Dropout(p=0.2)
 
-        self.maxpool1 = nn.MaxPool2d(2)
-        self.maxpool2 = nn.MaxPool2d(2)
+        self.maxpool1 = nn.MaxPool1d(2)
+        self.maxpool2 = nn.MaxPool1d(2)
+        self.maxpool3 = nn.MaxPool1d(2)
 
-        self.avgPool = nn.AvgPool2d(2)
-        self.dense = nn.Softmax(2)
+        self.dense1 = nn.Linear(28*3, 64)
+        self.dense2 = nn.Linear(64,2)
+        # self.dense3 = nn.Softmax(2)
 
     def forward(self, x):
+        print(x.size())
 
-        out1 = self.drop1(self.maxpool1(F.relu(self.conv1(x))))
-        out2 = self.drop2(self.maxpool2(F.relu(self.conv2(out1))))
-        return (self.dense(self.avgPool(out2)))
+        out1 = self.maxpool1(F.relu(self.conv1(x)))
+        out2 = self.maxpool2(F.relu(self.conv2(out1)))
+
+        out2 = out2.view(-1, 28*3 )
+        # out3 = self.maxpool3(F.relu(self.conv3(out2)))
+        out4 = self.dense2(self.drop1(F.relu(self.dense1(out2))))
+        return out4
