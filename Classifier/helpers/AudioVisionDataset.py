1ca1fdf0914296cbd23dec7279a9fd434d694fd1
Megnath <megnath@ualberta.ca>
2019-09-07 17:38:09 -0600

    working training code

--- Classifier/helpers/AudioVisionDataset.py
+++ Classifier/helpers/AudioVisionDataset.py
@@ -2,17 +2,22 @@ import torch
 from torch.utils import data
 import pandas as pd
 from ast import literal_eval
+import numpy as np
 
 class AudioVisionDataset(data.Dataset):
 
     def __init__(self, filename):
         featuresdf = pd.read_csv(filename)
-        self.data = featuresdf['feature'].apply(literal_eval)
-        self.target = featuresdf['siren_present']
+        self.data = featuresdf['feature']
+        self.target = featuresdf['class_id']
         self.n_samples = self.data.shape[0]
         
     def __len__(self):   # Length of the dataset.
         return self.n_samples
     
     def __getitem__(self, index):   # Function that returns one point and one label.
-        return torch.Tensor(self.data[index]), torch.Tensor(self.target[index], dtype=torch.long)
+        temp = np.asarray(literal_eval(self.data[index]))
+        feature = torch.from_numpy(temp).float().to(torch.device('cpu'))
+        # print(self.target[index])
+        # target = torch.Tensor(self.target[index], dtype=torch.int)
+        return feature, self.target[index]
--- Classifier/helpers/ModelLayout.py
+++ Classifier/helpers/ModelLayout.py
@@ -9,27 +9,29 @@ class AudioVisionModel(nn.Module):
 
     def __init__(self):
         super(AudioVisionModel, self).__init__()
-
-        self.conv1 = nn.Conv1d(1, 14, 1)
-        self.conv2 = nn.Conv1d(14, 28, 1)
+        self.seq1 = nn.Sequential()
+        self.conv1 = nn.Conv2d(1, 14, 2)
+        self.conv2 = nn.Conv2d(14, 28, 2)
+        self.conv3 = nn.Conv2d(28, 42, 2)
+        self.conv4 = nn.Conv2d(42, 56, 2)
 
         self.drop1 = nn.Dropout(p=0.2)
+        self.drop2 = nn.Dropout(p=0.2)
 
-        self.maxpool1 = nn.MaxPool1d(2)
-        self.maxpool2 = nn.MaxPool1d(2)
-        self.maxpool3 = nn.MaxPool1d(2)
+        self.maxpool1 = nn.MaxPool2d(2)
+        self.maxpool2 = nn.MaxPool2d(2)
+        self.maxpool3 = nn.MaxPool2d(2)
 
-        self.dense1 = nn.Linear(28*3, 64)
-        self.dense2 = nn.Linear(64,2)
-        # self.dense3 = nn.Softmax(2)
+        self.avgPool = nn.AvgPool2d(2)
+        self.dense1 = nn.Linear(28*2*26, 64)
+        self.dense2 = nn.Linear(64,10)
 
     def forward(self, x):
-        print(x.size())
 
         out1 = self.maxpool1(F.relu(self.conv1(x)))
         out2 = self.maxpool2(F.relu(self.conv2(out1)))
-
-        out2 = out2.view(-1, 28*3 )
+        
+        out3 = out2.view(-1, 28*2*26 )
         # out3 = self.maxpool3(F.relu(self.conv3(out2)))
-        out4 = self.dense2(self.drop1(F.relu(self.dense1(out2))))
+        out4 = self.dense2(self.drop1(F.relu(self.dense1(out3))))
         return out4
