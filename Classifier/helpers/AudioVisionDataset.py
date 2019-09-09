07958f9dbf9aa4e35805a490eac9404ade00c232
DeepLearning VM <google-dl-platform@googlegroups.com>
2019-09-09 00:54:24 +0000

    Changes to make it work with GPU

--- Classifier/helpers/AudioVisionDataset.py
+++ Classifier/helpers/AudioVisionDataset.py
@@ -6,18 +6,23 @@ import numpy as np
 
 class AudioVisionDataset(data.Dataset):
 
-    def __init__(self, filename):
+    dtype = torch.FloatTensor
+    
+    def __init__(self, filename, dtype):
         featuresdf = pd.read_csv(filename)
         self.data = featuresdf['feature']
         self.target = featuresdf['class_id']
         self.n_samples = self.data.shape[0]
+        self.dtype = dtype
         
     def __len__(self):   # Length of the dataset.
         return self.n_samples
     
     def __getitem__(self, index):   # Function that returns one point and one label.
         temp = np.asarray(literal_eval(self.data[index]))
-        feature = torch.from_numpy(temp).float().to(torch.device('cpu'))
+        feature = torch.from_numpy(temp).long().type(self.dtype)
         # print(self.target[index])
-        # target = torch.Tensor(self.target[index], dtype=torch.int)
-        return feature, self.target[index]
+        val = [self.target[index].item()]
+#         print(val)
+        target = torch.LongTensor(val).type(torch.cuda.LongTensor)
+        return feature, target
